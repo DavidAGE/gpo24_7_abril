51f637d324c98ddac3d9dc19b20b99ceb48c5839
David Alejandro Garcia Elizondo <davidgareli07@gmail.com>
2025-04-07 15:51:10 -0600

    Nueva clase cuadrado

--- Demo.py
+++ Demo.py
@@ -8,3 +8,8 @@ class Cuadrado:
 NumLado = Cuadrado(50)
 print ("Elarea del cuadrado es:", NumLado.area())
 
+#############################################
+lado = int(input("Ingresa el lado?"))
+mi_ejemplo = Cuadrado(lado)
+r = mi_ejemplo.area()
+print(f'El area es: {r}')
